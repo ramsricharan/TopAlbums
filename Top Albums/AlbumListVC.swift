1fce3180e2f465e75141d1063a79f36fb3d2d424
Ram <ramcharan.syr@gmail.com>
2020-03-07 14:08:59 -0800

    initial commit

--- Top Albums/AlbumListVC.swift
+++ Top Albums/AlbumListVC.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class AlbumListVC: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = .yellow
+        
     }
 
 
--- Top Albums/AppDelegate.swift
+++ Top Albums/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Launching App Programatically
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let rootViewController = AlbumListVC()
+        self.window?.rootViewController = rootViewController
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
