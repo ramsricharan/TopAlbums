953b9169f675027f798f38b987e7873704f69499
Ram <ramcharan.syr@gmail.com>
2020-03-07 13:54:57 -0800

    Initial Commit

---
+++ Top Albums/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Albums
+//
+//  Created by Ram Sri Charan on 3/7/20.
+//  Copyright © 2020 Ram' Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Top AlbumsTests/Top_AlbumsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Top_AlbumsTests.swift
+//  Top AlbumsTests
+//
+//  Created by Ram Sri Charan on 3/7/20.
+//  Copyright © 2020 Ram' Apps. All rights reserved.
+//
+
+import XCTest
+@testable import Top_Albums
+
+class Top_AlbumsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
